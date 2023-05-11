41a612b4ab021e35db6782610034db2ebc50154d
Segun Osiki <osiki.segun@gmail.com>
2023-05-11 17:53:38 +0100

    create Registration complete event class

---
+++ src/main/java/com/bookmania/BookMania/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.bookmania.BookMania.controller;
+
+import com.bookmania.BookMania.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    private final ApplicationEventPublisher publisher;
+}
