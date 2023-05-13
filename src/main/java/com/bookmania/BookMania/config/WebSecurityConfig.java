aed080c70dfebf8621de3b2434ecde884c390cbb
Segun Osiki <osiki.segun@gmail.com>
2023-05-13 07:14:30 +0100

    create websecurity config class

---
+++ src/main/java/com/bookmania/BookMania/config/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.bookmania.BookMania.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig {
+
+    private static final String [] WHITE_LIST_URL = {
+            "/register"
+    };
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(11);
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .antMatchers(WHITE_LIST_URL).permitAll();
+
+        return http.build();
+
+    }
+}
