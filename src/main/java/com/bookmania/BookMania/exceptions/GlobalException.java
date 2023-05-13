dd8272e5896eaa4718aa192af7ca0cce56401f71
Segun Osiki <osiki.segun@gmail.com>
2023-05-13 07:37:59 +0100

    create global exception handler class

---
+++ src/main/java/com/bookmania/BookMania/exceptions/GlobalException.java
@@ -0,0 +1,25 @@
+package com.bookmania.BookMania.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(EmailAlreadyExistException.class)
+    public ResponseEntity<?> handleEmailAlreadyExistException(EmailAlreadyExistException e,
+                                                              WebRequest request){
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), e.getMessage(),
+                request.getDescription(false));
+
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.CONFLICT);
+
+    }
+}
