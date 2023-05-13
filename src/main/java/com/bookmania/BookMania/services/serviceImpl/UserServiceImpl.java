74650fd3e10112070a5f0271588a6ded24059f43
Segun Osiki <osiki.segun@gmail.com>
2023-05-13 07:24:05 +0100

    create logic for user registration in user service implementation class

--- src/main/java/com/bookmania/BookMania/services/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/bookmania/BookMania/services/serviceImpl/UserServiceImpl.java
@@ -2,18 +2,36 @@ package com.bookmania.BookMania.services.serviceImpl;
 
 import com.bookmania.BookMania.dto.UserDto;
 import com.bookmania.BookMania.model.User;
+import com.bookmania.BookMania.repository.UserRepository;
 import com.bookmania.BookMania.services.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public User registerUser(UserDto userDto) {
-        return null;
+
+        User user = User.builder()
+                .firstname(userDto.getFirstname())
+                .lastname(userDto.getLastname())
+                .email(userDto.getEmail())
+                .role("USER")
+                .password(passwordEncoder.encode(userDto.getPassword()))
+                .build();
+
+        userRepository.save(user);
+
+        return user;
     }
+
+
     @Override
     public void saveVerificationTokenForUser(String token, User user) {
 
