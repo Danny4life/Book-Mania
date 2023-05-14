ac9c7e52fdbbf32de69d3c7ea8a2cb45a393c9e6
Segun Osiki <osiki.segun@gmail.com>
2023-05-14 09:23:19 +0100

    create email validator class

--- src/main/java/com/bookmania/BookMania/validations/EmailValidator.java
+++ src/main/java/com/bookmania/BookMania/validations/EmailValidator.java
@@ -2,11 +2,20 @@ package com.bookmania.BookMania.validations;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Pattern;
 
 public class EmailValidator implements ConstraintValidator<ValidEmail, String> {
 
+    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private Pattern pattern;
+
     @Override
-    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
-        return false;
+    public void initialize(ValidEmail constraintAnnotation) {
+        pattern = Pattern.compile(EMAIL_PATTERN);
+    }
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+        return email != null && pattern.matcher(email).matches();
     }
 }
