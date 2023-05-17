5b378267c2fd82ab45ac5ad080d23ae59afd7b99
Segun Osiki <osiki.segun@gmail.com>
2023-05-17 14:50:51 +0100

    create end point to get all books in the book controller class

--- src/main/java/com/bookmania/BookMania/controller/BookController.java
+++ src/main/java/com/bookmania/BookMania/controller/BookController.java
@@ -1,11 +1,14 @@
 package com.bookmania.BookMania.controller;
 
 import com.bookmania.BookMania.dto.BookDto;
+import com.bookmania.BookMania.model.Book;
 import com.bookmania.BookMania.services.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -28,6 +31,13 @@ public class BookController {
         bookDto = bookService.getBookById(id);
 
         return ResponseEntity.ok(bookDto);
+
+    }
+
+    @GetMapping("/get-all-books/{id}")
+
+    public List<Book> getAllBooks(@PathVariable Long id){
+        return bookService.getAllBooks(id);
 
     }
 }
--- src/main/java/com/bookmania/BookMania/services/BookService.java
+++ src/main/java/com/bookmania/BookMania/services/BookService.java
@@ -1,9 +1,14 @@
 package com.bookmania.BookMania.services;
 
 import com.bookmania.BookMania.dto.BookDto;
+import com.bookmania.BookMania.model.Book;
+
+import java.util.List;
 
 public interface BookService {
     BookDto createBook(Long id, BookDto bookDto);
 
     BookDto getBookById(Long id);
+
+    List<Book> getAllBooks(Long id);
 }
