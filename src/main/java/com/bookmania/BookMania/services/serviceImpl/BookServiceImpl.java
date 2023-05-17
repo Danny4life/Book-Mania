003c6901cd01f22089333dcd472e4e9ea927b8e7
Segun Osiki <osiki.segun@gmail.com>
2023-05-17 05:53:45 +0100

    create book service class

---
+++ src/main/java/com/bookmania/BookMania/services/serviceImpl/BookServiceImpl.java
@@ -0,0 +1,16 @@
+package com.bookmania.BookMania.services.serviceImpl;
+
+import com.bookmania.BookMania.repository.BookRepository;
+import com.bookmania.BookMania.repository.UserRepository;
+import com.bookmania.BookMania.services.BookService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class BookServiceImpl implements BookService {
+
+    private final UserRepository userRepository;
+    private final BookRepository bookRepository;
+
+}
