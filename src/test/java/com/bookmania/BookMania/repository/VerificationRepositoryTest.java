0499cb0e1ef25cee70e95c8c2db675040759cced
Segun Osiki <osiki.segun@gmail.com>
2023-05-24 16:09:30 +0100

    create verification repository test class

---
+++ src/test/java/com/bookmania/BookMania/repository/VerificationRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.bookmania.BookMania.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VerificationRepositoryTest {
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findByToken() {
+    }
+}
