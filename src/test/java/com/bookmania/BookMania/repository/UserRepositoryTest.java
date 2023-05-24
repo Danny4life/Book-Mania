c0f2484391984043e8c9d4f7adf9aef60db26151
Segun Osiki <osiki.segun@gmail.com>
2023-05-24 10:53:44 +0100

    write test to check if user email does not exists in the database

--- src/test/java/com/bookmania/BookMania/repository/UserRepositoryTest.java
+++ src/test/java/com/bookmania/BookMania/repository/UserRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.bookmania.BookMania.repository;
 
 import com.bookmania.BookMania.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,6 +15,11 @@ class UserRepositoryTest {
 
     @Autowired
     private UserRepository underTest;
+
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
 
     @Test
     void itShouldCheckIfUserEmailExists() {
@@ -32,6 +39,25 @@ class UserRepositoryTest {
         // Then
         assertTrue(userExists.isPresent());
         assertEquals("john@gmail.com", userExists.get().getEmail());
+    }
+
+    @Test
+    void itShouldCheckIfUserEmailDoesNotExists() {
+        // Given
+        User user = new User();
+        user.setFirstname("john");
+        user.setLastname("doe");
+        user.setEmail("john@gmail.com");
+        user.setPassword("password");
+        user.setRole("USER");
+        user.setEnabled(true);
+
+        // When
+        Optional<User> userExists = underTest.findByEmail("john@gmail.com");
+
+        // Then
+        assertFalse(userExists.isPresent());
+
     }
 
     @Test
