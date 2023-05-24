f806892f8ee7deb66bc3f859d8503bce15bfc5e2
Segun Osiki <osiki.segun@gmail.com>
2023-05-24 17:38:47 +0100

    write test to find by token when token exists should return password reset token

--- src/test/java/com/bookmania/BookMania/repository/PasswordResetTokenRepositoryTest.java
+++ src/test/java/com/bookmania/BookMania/repository/PasswordResetTokenRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.bookmania.BookMania.repository;
 
+import com.bookmania.BookMania.model.PasswordResetToken;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,12 +11,30 @@ import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 class PasswordResetTokenRepositoryTest {
 
+    @Autowired
+    private PasswordResetTokenRepository underTest;
+
 
     @AfterEach
     void tearDown() {
     }
 
     @Test
-    void findByToken() {
+    void testFindByToken_WhenTokenExists_ShouldReturnPasswordResetToken() {
+
+        // Given
+        String token = "token";
+        PasswordResetToken expectedToken = new PasswordResetToken();
+        expectedToken.setToken(token);
+
+        underTest.save(expectedToken);
+
+        // When
+        PasswordResetToken resultToken = underTest.findByToken(token);
+
+        // Then
+        assertNotNull(resultToken);
+        assertEquals(expectedToken, resultToken);
+
     }
 }
