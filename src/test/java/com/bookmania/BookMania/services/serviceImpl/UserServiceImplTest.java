d0244a37be4a635366f66cef9e69da9add83c2b1
Segun Osiki <osiki.segun@gmail.com>
2023-05-25 10:34:43 +0100

    create user service implementation test class

---
+++ src/test/java/com/bookmania/BookMania/services/serviceImpl/UserServiceImplTest.java
@@ -0,0 +1,62 @@
+package com.bookmania.BookMania.services.serviceImpl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void registerUser() {
+    }
+
+    @Test
+    void validateVerificationToken() {
+    }
+
+    @Test
+    void generateNewVerificationToken() {
+    }
+
+    @Test
+    void saveVerificationTokenForUser() {
+    }
+
+    @Test
+    void findUserByEmail() {
+    }
+
+    @Test
+    void createPasswordResetTokenForUser() {
+    }
+
+    @Test
+    void validatePasswordResetToken() {
+    }
+
+    @Test
+    void getUserByPasswordResetToken() {
+    }
+
+    @Test
+    void changePassword() {
+    }
+
+    @Test
+    void checkIfValidOldPassword() {
+    }
+
+    @Test
+    void loginUser() {
+    }
+}
